da9c641681dbd7fba04400130371d3a3be4e170f
francocas <franquicas@hotmail.com>
2017-05-20 16:10:43 -0200

    Add files via upload

---
+++ TpEstacionamiento/Clases/empleados.php
@@ -0,0 +1,7 @@
+<?php 
+class empleado
+{
+    public $nombre;
+    public $apellido;
+}
+?>
---
+++ TpEstacionamiento/Clases/vehiculo.php
@@ -0,0 +1,8 @@
+<?php
+class vehiculo
+{
+    public $patente;
+    public $color;
+    public $marca;
+}
+?>
