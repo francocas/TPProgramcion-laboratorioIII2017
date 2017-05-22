07c811eb79ebc3da9808a2afe07ed599c456f740
francocas <franquicas@hotmail.com>
2017-05-21 23:12:29 -0200

    Add files via upload

--- TpEstacionamiento/Clases/empleados.php
+++ TpEstacionamiento/Clases/empleados.php
@@ -1,7 +1,13 @@
 <?php 
 class empleado
 {
-    public $nombre;
-    public $apellido;
+    public $_nombre;
+    public $_apellido;
+
+     public function __construct($nombre, $apellido)
+     {
+         $_apellido = $apellido;
+         $_nombre = $nombre;
+     }
 }
 ?>
--- TpEstacionamiento/Clases/vehiculo.php
+++ TpEstacionamiento/Clases/vehiculo.php
@@ -1,8 +1,15 @@
 <?php
 class vehiculo
 {
-    public $patente;
-    public $color;
-    public $marca;
+    public $_patente;
+    public $_color;
+    public $_marca;
+
+    public function __construct($patente, $color, $marca)
+    {
+        $_color = $color;
+        $_marca = $marca;
+        $_patente = $patente;
+    }
 }
 ?>
