d329690916b85eeed67edb16ebeb6fe995a1e1d1
alumno <alumno>
2017-05-29 22:08:17 -0300

    41

--- TpParcial/TpEstacionamiento/consultas.php
+++ TpParcial/TpEstacionamiento/consultas.php
@@ -9,7 +9,7 @@
             $Auxiliar = $database->Query("SELECT * FROM 'lugares' WHERE lugares.FlagOcupado == 1");
             break;
         case 'ingreso':
-            $Auxiliar = $database->Query("UPDATE `lugares` SET `FlagOcupado`= 1,`PatenteAuto`=$_POST['Patente'],`ColorAuto`=$_POST['Color'],`ModeloAuto`=$_POST['Modelo'],`HorarioEntrada`="date("j.G:i")",`HorarioSalida`='' WHERE lugares.id == $_POST['id']");
+            $Auxiliar = $database->Query("UPDATE `lugares` SET `FlagOcupado`= 1,`PatenteAuto`=$_POST['Patente'],`ColorAuto`=$_POST['Color'],`ModeloAuto`=$_POST['Modelo'],`HorarioEntrada`=".date("j.G:i").",`HorarioSalida`='' WHERE lugares.id == $_POST['id']");
             break;
         case 'salida':
             $Auxiliar = $database->Query("INSERT INTO `informes`(`idLugar`, `PatenteAuto`, `ColorAuto`, `ModeloAuto`, `HorarioEntrada`, `HorarioSalida`, `UsuarioIngreso`, `UsuarioSalida`) VALUES (lugares.id,lugares.PatenteAuto,lugares.ColorAuto,lugares.ModeloAuto,lugares.HorarioEntrada,"date("j.G:i")",[value-7],[value-8])");
